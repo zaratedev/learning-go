3faa3ef898bc0318fe532a786e2b7255f7093fe0
Jonathan Zarate <zaratedev@gmail.com>
2024-02-13 12:31:28 -0600

    Constantes

--- main.go
+++ main.go
@@ -2,26 +2,8 @@ package main
 
 import "fmt"
 
+const Course string = "Curso de Go"
+
 func main() {
-	name := "Jonathan"
-
-	fmt.Println(name)
-
-	// Multiples variables
-
-	/**
-	var lastname string
-	var country string
-	*/
-
-	// Declaracion sin asignacion
-	// var lastname, country string
-
-	// Declaracion asignado valores
-	// var lastname, country = "Zarate Hernandez", "Mexico"
-
-	// Usando otra sintaxis
-	lastname, country := "Zarate Hernandez", "Mexico"
-
-	fmt.Println(lastname, country)
+	fmt.Println(Course)
 }
