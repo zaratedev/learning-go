219c700af00ae5f3f8d8699904cf789d7706d35e
Jonathan Zarate <zaratedev@gmail.com>
2024-02-16 13:04:18 -0600

    Ciclo for

--- main.go
+++ main.go
@@ -5,49 +5,22 @@ import (
 )
 
 func main() {
-	// Condicionales
-	var edad int
-	// fmt.Print("Ingresa tu edad: ")
-	// fmt.Scanf("%d", &edad)
+	// Ciclo for
 
-	edad = 18
-
-	if edad >= 18 {
-		fmt.Println("Usuario mayor de edad")
-	} else {
-		fmt.Println("Usuario menor de edad")
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
 	}
 
-	// Multiples condiciones
+	// For como while
+	number := 12345
+	count := 0
 
-	var calificacion int
-
-	fmt.Print("Ingresa una calificacion: ")
-	fmt.Scanf("%d", &calificacion)
-
-	if calificacion == 10 {
-		fmt.Println("Felicidades aprobaste la materia")
-	} else if calificacion == 8 || calificacion == 9 {
-		fmt.Println("Aprobaste la materia.")
-	} else if calificacion == 6 || calificacion == 7 {
-		fmt.Println("Aprobaste la materia. Pero necesitas estudiar mas")
-	} else if calificacion >= 0 && calificacion <= 5 {
-		fmt.Println("Reprobsate la materia")
-	} else {
-		fmt.Println("Calificación no valida")
+	for number > 0 {
+		number = number / 10
+		count++
 	}
 
-	// Usando switch
-	switch calificacion {
-	case 10:
-		fmt.Println("Felicidades aprobaste la materia")
-	case 8, 9:
-		fmt.Println("Aprobaste la materia.")
-	case 7, 6:
-		fmt.Println("Aprobaste la materia. Pero necesitas estudiar mas")
-	case 1, 2, 3, 4, 5:
-		fmt.Println("Reprobsate la materia")
-	default:
-		fmt.Println("Calificación no valida")
-	}
+	fmt.Println("Cantidad de digitos", count)
 }
