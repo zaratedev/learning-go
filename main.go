7716fc9edfc54454dd345597834c8e8dca4d6e1c
Jonathan Zarate <zaratedev@gmail.com>
2024-03-06 15:46:02 -0600

    Interfaces

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
-import "fmt"
-
-type UserType int
-
-const (
-	Teacher UserType = 1
-	Student UserType = 2
+import (
+	"fmt"
 )
 
-type User struct {
-	Username string
-	Type     UserType
+type Animal interface {
+	// Definir metodos
+	eat()
+	sleep()
 }
 
-func main() {
-	user1 := User{Username: "zaratedev", Type: Student}
-	user2 := User{Username: "Jonathan", Type: Teacher}
+type Dog struct {
+	Name string
+}
 
-	fmt.Println(user1)
-	fmt.Println(user2)
+func (dog Dog) eat() {
+	fmt.Println(dog.Name, "Esta comiendo")
+}
+
+func (dog Dog) sleep() {
+	fmt.Println(dog.Name, "Esta durmiendo")
+}
+
+func executeActions(animal Animal) {
+	animal.eat()
+	animal.sleep()
+}
+
+// Interfaces
+func main() {
+	dog := Dog{Name: "Docker"}
+
+	fmt.Println(dog.Name)
+
+	executeActions(dog)
 }
